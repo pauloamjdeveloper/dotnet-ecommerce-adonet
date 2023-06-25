b127225bfc46cd8053c041a6b6ae463e0e08c7fe
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-06-25 11:51:58 -0300

    Definindo as classes de modelo

---
+++ src/eCommerce.API/Models/Departamento.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace eCommerce.API.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public ICollection<Usuario> Usuarios { get; set; }
+    }
+}
