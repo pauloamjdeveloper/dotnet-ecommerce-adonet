c5777ca12de64c73f82f5cd57e8dc4a1277a8d11
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-06-25 12:36:58 -0300

    Definindo string de conexão e implementando a interface IUsuarioRepository

---
+++ src/eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.API.Models;
+using System.Collections.Generic;
+
+namespace eCommerce.API.Repositories
+{
+    internal interface IUsuarioRepository
+    {
+        public List<Usuario> Get();
+        public Usuario Get(int id);
+        public void Insert(Usuario usuario);
+        public void Update(Usuario usuario);
+        public void Delete(int id);
+    }
+}
---
+++ src/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.API.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+
+        public UsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerceDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public List<Usuario> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
