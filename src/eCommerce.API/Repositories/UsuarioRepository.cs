cc5937935700d39d988f3f82d31192040771bf7e
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-06-26 20:25:16 -0300

    Criando método para listar todos os usuários

--- src/eCommerce.API/Repositories/UsuarioRepository.cs
+++ src/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerce.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -16,7 +17,45 @@ namespace eCommerce.API.Repositories
 
         public List<Usuario> Get()
         {
-            throw new System.NotImplementedException();
+            var usuarios = new List<Usuario>();
+
+            try
+            {
+                var command = new SqlCommand();
+                command.CommandText = "SELECT * FROM Usuarios";
+                command.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    var usuario = new Usuario();
+
+                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.RG = dataReader.GetString("RG");
+                    usuario.CPF = dataReader.GetString("CPF");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+            finally 
+            {
+                _connection.Close();
+            }
+
+            return usuarios;
         }
 
         public Usuario Get(int id)
